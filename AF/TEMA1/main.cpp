35be7207b5afd19987579e537460faef201b46d4
Sebastian <sebastian16m16@gmail.com>
2019-10-14 16:43:41 +0300

    3 algorithms

--- AF/TEMA1/main.cpp
+++ AF/TEMA1/main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
+#include <cstdio>
 #include "Profiler.h"
 
 int n = 10;
 Profiler profiler("SortProfiler");
+//Operation bs1 = profiler.createOperation("BS-Moves", n);
+//Operation ss1 = profiler.createOperation("SS-Moves", n);
 
+void bubbleSort(int * arr, int x){
 
-void bubbleSort(int * arr, int n){
-    int size = n;
+    int size = x;
     int aux;
 
     while(size != 0) {
-        profiler.countOperation("Moves", n, 1);
         for (int j = 0; j < size; j++) {
+            profiler.countOperation("BS", x, 1);
             for (int i = 0; i < size - 1; i++) {
                 if (arr[i] > arr[i + 1]) {
                     aux = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = aux;
-                    profiler.countOperation("Assignations", n, 3);
-
+                    profiler.countOperation("BS", x, 3);
                 }
             }
             --size;
@@ -28,13 +28,14 @@ void bubbleSort(int * arr, int n){
     }
 }
 
-void selectionSort(int * arr, int n){
+void selectionSort(int * arr, int x){
     int min_index;
     int aux;
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<x; i++){
+        profiler.countOperation("SS", x, 1);
         min_index = i;
-        for(int j=i+1; j < n; j++){
+        for(int j=i+1; j < x; j++){
             if(arr[j] < arr[min_index]){
                 min_index = j;
             }
@@ -42,38 +43,132 @@ void selectionSort(int * arr, int n){
         aux = arr[i];
         arr[i] = arr[min_index];
         arr[min_index] = aux;
-
+        profiler.countOperation("SS", x, 3);
     }
 }
 
-void printArray(int * arr, int n){
-    for(int i=0; i<n; i++){
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    /* create temp arrays */
+    int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++){
+        profiler.countOperation("MS", r, 1);
+    L[i] = arr[l + i];
+    }
+    for (j = 0; j < n2; j++) {
+        profiler.countOperation("MS", r, 1);
+        R[j] = arr[m + 1+ j];
+    }
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            profiler.countOperation("MS", r, 1);
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            profiler.countOperation("MS", r, 1);
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there
+       are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        profiler.countOperation("MS", r, 1);
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there
+       are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        profiler.countOperation("MS", r, 1);
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l+(r-l)/2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+void printArray(int * arr, int x){
+    for(int i=0; i<x; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
+void copyArrayAtoB(int *a, int *b, int x){
+    for(int i=0; i < x; i++){
+        b[i] = a[i];
+    }
+}
+
 int main() {
 
-    int a[n];
-    int b[n];
-    FillRandomArray(b, n, 0, 60, false, 0);
+    int a[1000];
+    int b[1000];
+    int c[1000];
+    FillRandomArray(a, 1000, 1, 60, false, 0);
+    copyArrayAtoB(a, b, 1000);
+    copyArrayAtoB(a, c, 1000);
 
-    printf("Bubble\n");
+        printf("THE ARRAY: \n");
+        printArray(a, n);
+        printf("\n");
+        printf("\n");
 
-    for(int i = 0; i < 10000; i++){
-        FillRandomArray(a, n, 0, 60, false, 0);
-        bubbleSort(a, n);
-
+    for(int j=0; j <= 1000; j++) {
+        for (int i = 10; i < 1000; i += 10) {
+            bubbleSort(a, i);
+            selectionSort(b, i);
+            mergeSort(c, 0, i);
+        }
     }
-    printArray(a, n);
+    printArray(a, 1000);
+    printf("\n");
+    printf("\n");
+    printArray(b, 1000);
+    printf("\n");
+    printf("\n");
+    printArray(c, 1000);
+    printf("\n");
+    printf("\n");
 
-    printf("SelectionSort\n");
-    printArray(b, n);
-    selectionSort(b, n);
-    printArray(b, n);
-
-    profiler.createGroup("BSearch", "Moves", "Assignations");
+    profiler.createGroup("BSearch", "BS", "SS", "MS");
     profiler.showReport();
 
     return 0;
